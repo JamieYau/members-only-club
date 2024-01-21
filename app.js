32a07a7b112647d55f41b8c548ad17e2b9170ef1
JamieYau <jamie.yau2002@me.com>
2024-01-21 17:59:49 +0000

    Add database connection and new files

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+// connect to database
 mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URI)
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log(err));
 
