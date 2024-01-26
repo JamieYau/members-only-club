34088ac9492d23c57eabbabee7e89f2499f39805
JamieYau <jamie.yau2002@me.com>
2024-01-26 22:04:57 +0000

    Add logout functionality

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET logout page.
+router.get("/logout", (req, res, next) => {
+  // Log out the user
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    // Render the logout page
+    res.render("logout");
+  });
+});
+
 module.exports = router;
